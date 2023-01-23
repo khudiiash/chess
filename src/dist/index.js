77c4aa1441f929ee6101d5220b92bd7aeb3b0497
Khudiiash <dmytro.khudiyash@gmail.com>
2023-01-23 16:11:42 +0200

    update server paths

--- src/dist/index.js
+++ src/dist/index.js
@@ -19,23 +19,35 @@ const http_1 = __importDefault(require("http"));
 const handler_1 = __importDefault(require("./handler"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3000;
 app.use((0, cors_1.default)());
 const server = http_1.default.createServer(app);
 const io = new socket_io_1.Server(server);
 new handler_1.default(io);
-app.use(express_1.default.static(path_1.default.join(__dirname, 'client', 'dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, 'client', 'dist', 'index.html'));
-});
+const paths = {
+    client: {
+        prod: path_1.default.join(__dirname, '..', 'client', 'dist'),
+        dev: path_1.default.join(__dirname, 'client', 'dist'),
+    }
+};
 const run = () => __awaiter(void 0, void 0, void 0, function* () {
     if (process.env.NODE_ENV === 'production') {
+        // @ts-ignore
+        const port = `0.0.0.0:${process.env.PORT}`;
+        app.use(express_1.default.static(paths.client.prod));
+        app.get('*', (req, res) => {
+            res.sendFile(path_1.default.join(paths.client.prod, 'index.html'));
+        });
         server.listen(port, () => {
             console.log(`Production server is up on port ${port}`);
         });
     }
     else {
         const ngrok = require('ngrok');
+        const port = 3000;
+        app.use(express_1.default.static(paths.client.dev));
+        app.get('*', (req, res) => {
+            res.sendFile(path_1.default.join(paths.client.dev, 'index.html'));
+        });
         const url = yield ngrok.connect(port);
         server.listen(port, () => {
             console.log(`Development server is up on port ${port} and ngrok url is ${url}`);
--- src/index.ts
+++ src/index.ts
@@ -11,22 +11,31 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server);
 new UserHandler(io);
-
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+const paths = {
+    client: {
+        prod: path.join(__dirname, '..', 'client', 'dist'),
+        dev: path.join(__dirname, 'client', 'dist'),
+    }
+}
 
 const run = async() => {
     if (process.env.NODE_ENV === 'production') {
         // @ts-ignore
         const port = `0.0.0.0:${process.env.PORT}`;
+        app.use(express.static(paths.client.prod));
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(paths.client.prod, 'index.html'));
+        });
         server.listen(port, () => {
             console.log(`Production server is up on port ${port}`);
         });
     } else {
         const ngrok = require('ngrok');
         const port = 3000;
+        app.use(express.static(paths.client.dev));
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(paths.client.dev, 'index.html'));
+        });
         const url = await ngrok.connect(port);
         server.listen(port, () => {
             console.log(`Development server is up on port ${port} and ngrok url is ${url}`);
